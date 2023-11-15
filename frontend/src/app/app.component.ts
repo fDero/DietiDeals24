33fcb98209d51d0d718c3bfb47901d43ec24e3dd
fDero <francescodero@outlook.it>
2023-11-15 18:22:26 +0100

    added default redirect to login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 
@@ -12,4 +12,17 @@ import { RegisterPageComponent } from './register-page/register-page.component';
 })
 export class AppComponent {
 	
+	private router : Router
+
+	constructor(router : Router) {
+		this.router = router
+	}
+
+	ngOnInit() : void {
+    	this.router.events.subscribe((event) => {
+			if (event instanceof NavigationEnd && this.router.url === '/') {	
+				this.router.navigate(['/login']);
+			}
+    	})
+  	}
 }
